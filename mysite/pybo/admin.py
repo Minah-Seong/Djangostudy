44f103cc7447c15ebae31ba76e43fc1d46650e95
Minah <rinette9612@gmail.com>
2022-02-26 15:02:49 +0900

    장고 관리자

--- mysite/pybo/admin.py
+++ mysite/pybo/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Question
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+# 관리자 화면에서 제목으로 질문 검색
+# Question 모델에 세부 기능을 추가할 수 있는 클래스 생성
+# 제목 검색을 위한 search_fields 속성에 'subject' 추가
+
+admin.site.register(Question, QuestionAdmin)
 
 # Register your models here.
+# 다른 장고 관리자 속성은 검색
